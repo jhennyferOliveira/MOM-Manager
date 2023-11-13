b61e8858b9d95c3a2194ab9aaa7bc29ce0eb28eb
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2023-11-13 20:59:31 -0300

    Adding connection to activemq

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
+
+import network.ConnectionHandler;
 import view.BrokerMainView;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ConnectionHandler.connectToActiveMQ();
         SwingUtilities.invokeLater(() -> {
             try {
                 var view = new BrokerMainView();
